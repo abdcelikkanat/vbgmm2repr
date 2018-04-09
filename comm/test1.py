b347f7b4c8f83162769cc78a6f735470cfdd540a
Abdulkadir Celikkanat <abdcelikkanat@gmail.com>
2018-04-09 09:44:54 +0200

    msg

--- comm/test1.py
+++ comm/test1.py
@@ -2,13 +2,13 @@ import numpy as np
 import networkx as nx
 
 
-F = np.load("./numpy_files/citeseer_poisson_model1_iter_50.npy")
+F = np.load("./numpy_files/citeseer_sigmoid2_nb2_iter_500_T.npy")
 
 g = nx.read_gml("../datasets/citeseer.gml")
 
 n = g.number_of_nodes()
 
-with open("citeseer_poisson_model1_iter_50.embedding", 'w') as f:
+with open("citeseer_sigmoid2_nb2_iter_500_T.embedding", 'w') as f:
     f.write("{} {}\n".format(n, F.shape[1]))
     for node in g.nodes():
         line = [str(val) for val in F[int(node), :]]
